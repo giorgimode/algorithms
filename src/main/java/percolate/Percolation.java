88bf0e84dfc20c2ea3b52961d2a0d1dff1448350
Giorgi <modegiorgi@gmail.com>
2016-04-26 23:50:39 +0200

    percolation finished

--- src/main/java/percolate/Percolation.java
+++ src/main/java/percolate/Percolation.java
@@ -1,4 +1,5 @@
 package main.java.percolate;
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
@@ -11,12 +12,13 @@ public class Percolation {
     private WeightedQuickUnionUF uf2;
 
     public Percolation(int n) {
-        if (n <= 0) throw new IllegalArgumentException("Grid size must be a positive integer");
+        if (n <= 0) throw
+                new IllegalArgumentException("Grid size must be a positive integer");
         N = n;
         // last element n*n
-        grid1DIsOpen = new boolean[n * n+1];
+        grid1DIsOpen = new boolean[n * n + 1];
         // last element is n*n, but 0 and (n*n+1) are virtual roots
-        uf = new WeightedQuickUnionUF(n * n + 2 );
+        uf = new WeightedQuickUnionUF(n * n + 2);
         uf2 = new WeightedQuickUnionUF(n * n + 1);
 
     }
@@ -24,19 +26,17 @@ public class Percolation {
     public void open(int x, int y) {
         int convertedIndex;
 
-        if (!isOpen(x,y)) {
+        if (!isOpen(x, y)) {
             convertedIndex = get1DIndex(x, y);
             grid1DIsOpen[convertedIndex] = true;
 
-            if(convertedIndex>0 && convertedIndex<=N)
-            { uf.union(convertedIndex, 0);
+            if (convertedIndex > 0 && convertedIndex <= N) {
+                uf.union(convertedIndex, 0);
                 uf2.union(convertedIndex, 0);
+            } else if (convertedIndex > N * (N - 1) && convertedIndex <= N * N) {
+                uf.union(convertedIndex, N * N + 1);
             }
-            else     if(convertedIndex>N*(N-1) && convertedIndex<=N*N){
-                uf.union(convertedIndex, N*N+1);
-            }
-        }
-        else return;
+        } else return;
 
         // link to upper site if it exists
         if (validate(x, y - 1) && grid1DIsOpen[get1DIndex(x, y - 1)]) {
@@ -44,7 +44,6 @@ public class Percolation {
             uf2.union(convertedIndex, get1DIndex(x, y - 1));
 
         }
-
 
 
         // link to bottom site if it exists
@@ -70,27 +69,27 @@ public class Percolation {
 
     }
 
-    public boolean isOpen(int x, int y){
+    public boolean isOpen(int x, int y) {
         int convertedIndex;
 
         if (validate(x, y)) {
             convertedIndex = get1DIndex(x, y);
-        } else throw new IndexOutOfBoundsException ("row index out of bounds");
+        } else throw new IndexOutOfBoundsException("row index out of bounds");
 
         return grid1DIsOpen[convertedIndex];
     }
 
     public boolean isFull(int x, int y) {
         int convertedIndex;
-            if (validate(x, y)) {
+        if (validate(x, y)) {
             convertedIndex = get1DIndex(x, y);
-        } else throw new IndexOutOfBoundsException ("row index out of bounds");
+        } else throw new IndexOutOfBoundsException("row index out of bounds");
 
         return uf2.connected(convertedIndex, 0);
     }
 
-    public boolean percolates(){
-        return uf.connected(0, N*N+1);
+    public boolean percolates() {
+        return uf.connected(0, N * N + 1);
     }
 
     // validate that p is a valid index
---
+++ src/main/java/percolate/PercolationStats.java
@@ -0,0 +1,65 @@
+package main.java.percolate;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private double[] results;
+    private Percolation percolation;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        if (N < 1 || T < 1) {
+            throw new IllegalArgumentException();
+        }
+        results = new double[T];
+        calculateTreshold(N, T);
+    }
+
+    private void calculateTreshold(int N, int T) {
+        for (int i = 0; i < T; i++) {
+            percolation = new Percolation(N);
+            int count = 0;
+            while (!percolation.percolates()) {
+                int randX = StdRandom.uniform(1, N + 1);
+                int randY = StdRandom.uniform(1, N + 1);
+                if (!percolation.isOpen(randX, randY)) {
+                    percolation.open(randX, randY);
+                    count++;
+                }
+            }
+            results[i] = count / (double) (N * N);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(results);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return results.length == 1 ? Double.NaN : StdStats.stddev(results);
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(results.length);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+
+        return mean() + 1.96 * stddev() / Math.sqrt(results.length);
+    }
+
+    // test client (described below)
+    public static void main(String[] args) {
+        PercolationStats stats = new PercolationStats(200, 100);
+        System.out.println("mean                           " + stats.mean());
+        System.out.println("stddev                         " + stats.stddev());
+        System.out.println("95% confidence interval        " + stats.confidenceLo() +
+                ", " + stats.confidenceHi());
+
+    }
+}
