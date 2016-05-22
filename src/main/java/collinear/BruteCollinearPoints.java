0f827259628a589f7bd551a04c33515844d68675
Giorgi <modegiorgi@gmail.com>
2016-05-22 23:31:04 +0200

    collinear - brute

---
+++ src/main/java/collinear/BruteCollinearPoints.java
@@ -0,0 +1,90 @@
+package main.java.collinear;
+
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+    private LineSegment[] lineSegments = new LineSegment[2];
+    private int segmentSize = 0;
+
+    public BruteCollinearPoints(Point[] points) {
+        // System.out.println(Arrays.asList(points));
+
+        Arrays.sort(points);
+        //   System.out.println(Arrays.asList(points));
+        //   removeDuplicates(points);
+
+        for (int i = 0; i < points.length - 3; i++) {
+            for (int j = i + 1; j < points.length - 2; j++) {
+                for (int k = j + 1; k < points.length - 1; k++) {
+                    if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[k])) {
+                        for (int l = k + 1; l < points.length; l++) {
+                            if (points[i].slopeTo(points[j]) == points[i].slopeTo(points[l])) {
+                                lineSegments[segmentSize++] = new LineSegment(points[i], points[l]);
+                                if (segmentSize == lineSegments.length)
+                                    resize(lineSegments.length * 2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void removeDuplicates(Point[] points) {
+        for (int i = 1; i < points.length; i++) {
+            if (points[i].compareTo(points[i - 1]) == 0)
+                throw new IllegalArgumentException("Duplicated entries in given points.");
+        }
+    }
+
+    public int numberOfSegments() {
+        return segmentSize;
+    }
+
+    public LineSegment[] segments() {
+        resize(segmentSize);
+        return lineSegments;
+    }
+
+    private void resize(int max) {
+        LineSegment[] newItems = new LineSegment[max];
+        for (int i = 0; i < segmentSize; i++) {
+            newItems[i] = lineSegments[i];
+        }
+        lineSegments = newItems;
+    }
+
+    public static void main(String[] args) {
+
+        // read the N points from a file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.show(0);
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+    }
+}
---
+++ src/main/java/collinear/Point.java
@@ -0,0 +1,101 @@
+package main.java.collinear;
+
+
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+    public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
+        if (that.compareTo(this) == 0) return Double.NEGATIVE_INFINITY;
+        if (that.y == y) return Double.POSITIVE_INFINITY;
+        if (that.x == x) return +0.0;
+
+        return (that.y - y) / (double) (that.x - x);
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
+     */
+    public int compareTo(Point that) {
+        if (y > that.y) return 1;
+        else if (y < that.y) return -1;
+        else {
+            if (x > that.x) return 1;
+            if (x < that.x) return -1;
+        }
+        return 0;
+    }
+
+    public Comparator<Point> slopeOrder() {
+        return new SlopeOrder();
+    }
+
+    private class SlopeOrder implements Comparator<Point> {
+        @Override
+        public int compare(Point q1, Point q2) {
+            if (slopeTo(q1) > slopeTo(q2)) return 1;
+            else if (slopeTo(q1) < slopeTo(q2)) return -1;
+            return 0;
+        }
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public static void main(String[] args) {
+        Point point0 = new Point(0, 5);
+        Point point1 = new Point(0, 5);
+        Point point2 = new Point(0, 0);
+        Point point3 = new Point(5, 5);
+
+        System.out.println(point0.compareTo(point1));
+        System.out.println(point0.compareTo(point2));
+        System.out.println(point0.compareTo(point3));
+        System.out.println(point3.compareTo(point0));
+
+        System.out.println(point0.slopeTo(point1));
+        System.out.println(point0.slopeTo(point2));
+        System.out.println(point0.slopeTo(point3));
+        System.out.println(point2.slopeTo(point3));
+
+    }
+}
